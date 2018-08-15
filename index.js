57504440f90944e4d0589dce3ac67f60ae087114
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2018-08-15 22:09:46 +1000

    added feature to accept options

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // @flow
 const grayMatter = require("gray-matter");
+const loaderUtils = require("loader-utils");
 module.exports = function(source /*: string*/) /*: string*/ {
+  const opts = loaderUtils.getOptions(this) || {};
+
   return `export default ${JSON.stringify(
-    grayMatter(source, { excerpt: true })
+    grayMatter(source, opts)
   )}`;
 };

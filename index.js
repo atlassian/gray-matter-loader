cafd5ac449fe12bbed733770db3bac7f27f5dbdd
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2018-08-18 07:05:26 +1000

    added keywords and formatted code

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ const grayMatter = require("gray-matter");
 const getOptions = require("loader-utils").getOptions;
 const validateOptions = require("schema-utils");
 
+const grayMatterOptionsType = {
+  excerpt: {
+    anyOf: [{ type: "boolean" }, { instanceof: "Function" }]
+  },
+  excerpt_separator: {
+    type: "string"
+  },
+  engines: {
+    type: "object"
+  },
+  language: {
+    type: "string"
+  },
+  delimiters: {
+    type: "string"
+  }
+};
+
 const optionsSchema = {
   type: "object",
-  properties: {
-    excerpt: {
-      anyOf: [{ type: "boolean" }, { instanceof: "Function" }]
-    },
-    excerpt_separator: {
-      type: "string"
-    },
-    engines: {
-      type: "object"
-    },
-    language: {
-      type: "string"
-    },
-    delimiters: {
-      type: "string"
-    }
-  }
+  properties: grayMatterOptionsType
 };
 
 function purifyOptions(options) {
   let opts = {};
-  const validOptions = Object.keys(optionsSchema.properties);
+  const validOptions = Object.keys(grayMatterOptionsType);
 
   validOptions.forEach(validOption => {
     if (options.hasOwnProperty(validOption)) {

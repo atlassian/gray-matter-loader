853f23ed3d02714d3f7595425a4e4d25d49a44e6
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2019-01-19 21:46:31 +1100

    fix type in tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
 // @flow
-const grayMatther = require("gray-matter");
+const grayMatter = require("gray-matter");
 const loader = require("../index");
 
 jest.mock("gray-matter");
@@ -7,10 +7,10 @@ jest.mock("gray-matter");
 describe("gray-matter s default export function", () => {
   it("should be called when no options are passes", () => {
     loader("test");
-    expect(grayMatther).toHaveBeenCalledWith("test", {});
+    expect(grayMatter).toHaveBeenCalledWith("test", {});
   });
   it("should be called with options that are passed through", () => {
     loader("test");
-    expect(grayMatther).toHaveBeenCalledWith("test", {});
+    expect(grayMatter).toHaveBeenCalledWith("test", {});
   });
 });
